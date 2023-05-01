cb65f494a369ca79f4891d30d077bee947794839
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-05-01 18:04:25 -0500

    task 8: Ambient Namespaces

---
+++ 0x00-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,17 @@
+/*
+that you build an interface and a type
+
+  * Create a type RowID and set it equal to number
+  * Create an interface RowElement that contains these 3 fields:
+    - firstName: string
+    - lastName: string
+    - age?: number
+*/
+
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
+
+export type RowID = number;
