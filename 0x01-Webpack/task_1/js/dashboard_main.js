5fee8b9ac4a08a7e48ed1060c687601418d73b51
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-05-06 00:20:44 -0500

    task 1

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,13 +1,3 @@
-/*
-The file should import jquery and add the below elements in the following order:
-
-    Paragraph element: Holberton Dashboard
-    Paragraph element: Dashboard data for the students
-    Button element with the text Click here to get started
-    Add a paragraph element as so <p id='count'></p>
-    Add another paragraph: Copyright - Holberton School
-*/
-
 import $ from 'jquery';
 import _ from 'lodash';
 
