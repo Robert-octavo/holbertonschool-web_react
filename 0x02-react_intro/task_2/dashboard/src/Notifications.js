0f5423a600f2842b569beda3a7473aa71593e6f1
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-05-08 22:44:23 -0500

    task 2 -  Modify the App

---
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,21 @@
+/*
+
+    It should import React
+    It should export a function
+    The function should return a div with the class Notifications
+    The div should contain a paragraph with the text Here is the list of notifications
+    import the file Notifications.css.
+*/
+
+import React from 'react';
+import './Notifications.css';
+
+const Notifications = () => {
+  return (
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+    </div>
+  )
+}
+
+export default Notifications;
