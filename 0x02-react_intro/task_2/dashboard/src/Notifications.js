8148e4a0ab64c7a7c76ba69f30447931547320ba
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-05-09 21:04:37 -0500

    task 3. Modify the Notifications

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -8,12 +8,38 @@
 */
 
 import React from 'react';
+import { getLatestNotification } from './utils';
 import './Notifications.css';
+import iconClose from './close-icon.png';
+
+
+const printMessage = () => {
+  console.log('Close button has been clicked');
+}
+
+const myStyle = {
+  position: 'relative',
+  float: 'right',
+  top: '-60px',
+  right: '-10px',
+  background: 'transparent',
+  border: 'none',
+  cursor: 'pointer',
+  outline: 'none',
+}
 
 const Notifications = () => {
   return (
     <div className="Notifications">
       <p>Here is the list of notifications</p>
+      <button style={myStyle} aria-label='Close' onClick={printMessage}>
+        <img src={iconClose} alt="Close" />
+      </button>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+      </ul>
     </div>
   )
 }
