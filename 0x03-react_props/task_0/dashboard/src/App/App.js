84520624e4b8963b900e57ea662ad554b98263e3
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-05-13 22:45:55 -0500

    Task 0: Update index and App.js to use the components

--- 0x03-react_props/task_0/dashboard/src/App/App.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -4,14 +4,18 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Login />
-      <Footer />
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <Notifications />
+        <Header />
+        <Login />
+        <Footer />
+      </div>
+    </React.Fragment>
   );
 }
 
