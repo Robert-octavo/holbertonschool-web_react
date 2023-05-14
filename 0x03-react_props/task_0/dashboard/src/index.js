01c708d8be0a59eeb5512f85a935ba9b501dec72
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-05-13 21:44:23 -0500

    previous files

---
+++ 0x03-react_props/task_0/dashboard/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import App from './App/App';
+import Notifications from './Notifications/Notifications';
+import reportWebVitals from './reportWebVitals';
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <div className='root-notifications'>
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
