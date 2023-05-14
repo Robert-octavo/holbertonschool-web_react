62dffa16e2cc630c75c4b1179bed065879429037
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-05-13 22:36:33 -0500

    add the login component to the App

--- 0x03-react_props/task_0/dashboard/src/App/App.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -3,19 +3,13 @@ import React from 'react';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Password: </label>
-        <input type="password" id="password" name="password" />
-        <button type="button">OK</button>
-      </div>
+      <Login />
       <Footer />
     </div>
   );
