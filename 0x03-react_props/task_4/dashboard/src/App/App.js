532990e417c690a29ed59cd8b03276d66c86b6f5
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-05-15 21:12:34 -0500

    task 4- Modify the App component

--- 0x03-react_props/task_4/dashboard/src/App/App.js
+++ 0x03-react_props/task_4/dashboard/src/App/App.js
@@ -1,22 +1,38 @@
 import './App.css';
 import React from 'react';
+import PropTypes from 'prop-types'; 
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
 
-function App() {
+function App( { isLoggedIn } ) {
+  /*When isLoggedIn is false, display the Login screen
+When isLoggedIn is true, display the CourseList screen*/ 
+  let login = isLoggedIn ? <CourseList /> : <Login />;
   return (
     <React.Fragment>
       <div className="App">
         <Notifications />
         <Header />
-        <Login />
+        <div>{ login }</div>
         <Footer />
       </div>
     </React.Fragment>
   );
 }
+
+/*
+Create a property isLoggedIn. It should be false by default
+*/
+App.propTypes = {
+  isLoggedIn: PropTypes.bool
+};
+
+App.defaultProps = {
+  isLoggedIn: false
+};
 
 export default App;
--- 0x03-react_props/task_4/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_4/dashboard/src/App/App.test.js
@@ -16,6 +16,7 @@ import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
 import { shallow } from 'enzyme';
 
 describe('App', () => {
@@ -42,5 +43,31 @@ describe('App', () => {
   it('render Footer component', () => {
     const wrapper = shallow(<Footer />);
     expect(wrapper.exists());
+  });
+});
+
+/*Add a test to check that CourseList is not displayed*/
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  it('CourseList is not displayed', () => {
+    expect(wrapper.find(CourseList)).toHaveLength(0);
+  });
+});
+
+/*Describe a new case, when isLoggedIn is true, and add two checks. The first one should verify that the Login component is not included. The second one should verify that the CourseList component is included.*/
+
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App isLoggedIn={true} />);
+  });
+  it('CourseList is displayed', () => {
+    expect(wrapper.find(CourseList)).toHaveLength(1);
+  });
+  it('Login is not displayed', () => {
+    expect(wrapper.find(Login)).toHaveLength(0);
   });
 });
