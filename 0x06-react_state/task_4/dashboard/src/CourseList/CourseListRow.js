d834c88a8ed2546c127242a5dc1bf5d88b654129
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-01 22:19:47 -0500

    task 4. Introduction to react hook

--- 0x06-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x06-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState} from "react";
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+
+  const [isCheck, setIsCheck] = useState(false);
+
+  // const checkChange = () => {
+  //   if (isCheck === false) {
+  //     setIsCheck(true);
+  //   } else {
+  //     setIsCheck(false);
+  //   }
+  // }
+
+  handleClick = () => {
+    setIsCheck(!isCheck);
+  }
+
+  const row = css(isCheck ? styles.rowChecked : null);
+
   if (isHeader === true) {
       if (textSecondCell === null) {
         return (
@@ -22,8 +39,13 @@ function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
     } else {
       return (
         <tr className={css(styles.rowColor)}>
-          <td>{textFirstCell}</td>
-          <td>{textSecondCell}</td>
+          <td className={row}>
+            <input type="checkbox" onClick={handleClick}/>
+            {textFirstCell}
+          </td>
+          <td>
+            {textSecondCell}
+          </td>
         </tr>
       );
     }
@@ -38,6 +60,9 @@ const styles = StyleSheet.create({
   headerStyle: {
     backgroundColor: '#deb5b545',
     border: '1px solid lightgray'
+  },
+  rowChecked: {
+    backgroundColor: '#e6e4e4'
   }
 });
 
