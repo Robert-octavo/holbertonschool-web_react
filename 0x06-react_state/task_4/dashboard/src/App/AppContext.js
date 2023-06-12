ad2c9fe42243e0ca2dbeb456ee06fbf8200b08a8
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-12 08:45:11 -0500

    file

--- 0x06-react_state/task_4/dashboard/src/App/AppContext.js
+++ 0x06-react_state/task_4/dashboard/src/App/AppContext.js
@@ -1,5 +1,4 @@
 /*
-
     Define a default user object, containing email, password, and isLoggedIn
     Define a default logOut function
     Create a React context containing a user object and a logOut function. Set both default values using the previously created elements
