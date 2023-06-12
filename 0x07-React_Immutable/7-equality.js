87bf545ec4a74fcb1b45a03d15b18bfa64d092e9
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-12 09:38:33 -0500

    task 7. Equality

---
+++ 0x07-React_Immutable/7-equality.js
@@ -0,0 +1,19 @@
+import { Map } from 'immutable';
+
+export const areMapsEqual = (map1, map2) => {
+  return map1.equals(map2);
+}
+
+const map1 = Map({
+  one: 'one',
+  two: 'two',
+  three: 'three',
+});
+
+const map2 = Map({
+  one: 'one',
+  two: 'two',
+  three: 'three',
+});
+
+console.log(areMapsEqual(map1, map2));
