bded77cf3a0175e5ca658d5ebfa1314b80e2ce5e
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-14 22:38:30 -0500

    use fromJS

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const map = Map(object);
+  const map = fromJS(object);
   return map.getIn(array, undefined);
 }
 
