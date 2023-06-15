522e863c62d13b8c2951101610cec265fba3255a
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-14 23:08:23 -0500

    test eslint

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,33 +1,12 @@
-import { Seq } from "immutable";
-/*
-Create a function named printBestStudents:
+import { Seq } from 'immutable';
 
-    It accepts one object as argument. The object is formed with the following structure:
 
-const grades = {
-  1: {
-    score: 99,
-    firstName: 'guillaume',
-    lastName: 'salva',
-  }
-};
-
-    Calling the function should filter any student with a score < 70 and print to the console the first name and the last name with the first letter capitalized:
-
-{
-    "1": { score: 99, firstName: "Guillaume", lastName: "Salva" },
-}
-*/
-
-export const printBestStudents = (grades) => {
+export default function printBestStudents(grades) {
   const seq = Seq(grades);
   const filtered = seq.filter((student) => {
-    //student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1).toLowerCase();
-    //student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1).toLowerCase();
     return student.score > 70
   });
 
-  /*change the firsletter of the first to uppercase*/
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   }
@@ -35,9 +14,11 @@ export const printBestStudents = (grades) => {
   const obj = filtered.toJS();
 
   for (const key in obj) {
-    obj[key].firstName = capitalizeFirstLetter(obj[key].firstName);
-    obj[key].lastName = capitalizeFirstLetter(obj[key].lastName);
-  }
+    if (key) {
+      obj[key].firstName = capitalizeFirstLetter(obj[key].firstName);
+      obj[key].lastName = capitalizeFirstLetter(obj[key].lastName);
+    }
+  };
 
   console.log(obj);
 
@@ -59,7 +40,7 @@ const grades = {
     score: 80,
     firstName: 'tesT',
     lastName: 'tEst',
-  }
+  },
 };
 
 printBestStudents(grades);
