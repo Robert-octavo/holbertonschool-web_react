dfc84792e91da1a9569457cbd9ce42e72e76178c
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-15 22:43:44 -0500

    task 6 - add some test

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -42,3 +42,67 @@ describe("uiActionCreators", () => {
     });
   });
 });
+
+/*
+test suite for the loginRequest action:
+
+    the first test should verify that if the API returns the right response, the store received two actions LOGIN and LOGING_SUCCESS
+    the first test should verify that if the API query fails, the store received two actions LOGIN and LOGIN_FAILURE
+
+Tips:
+
+    You can use node-fetch to query an API
+    You can install redux-mock-store and fetch-mock to simular the API and simulate the store
+    With fetch-mock, you can use getOnce and get to simulate success and failures
+*/
+
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { loginRequest, login, loginFailure, loginSuccess } from './uiActionCreators';
+import { LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE } from './uiActionTypes';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('loginRequest', () => {
+  it('login success', () => {
+    fetchMock.get('http://localhost:3000/login-success.json', {
+      body: {
+        email: 'johann.salva@holberton.nz',
+        password: 'test',
+      },
+      headers: { 'content-type': 'application/json' },
+    });
+
+    const expectedActions = [
+      dispatch(loginSuccess('email', 'password')),
+    ];
+
+    const store = mockStore({});
+    return store.dispatch(loginRequest('email', 'password')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    }
+    );
+  });
+
+  it('login failure', () => {
+    fetchMock.get('http://localhost:3000/login-success.json', {
+      body: {
+        email: 'test@test.com',
+        password: 'test',
+      },
+      headers: { 'content-type': 'application/json' },
+    });
+
+    const expectedActions = [
+      dispatch(loginFailure()),
+    ];
+
+    const store = mockStore({});
+    return store.dispatch(loginRequest('email', 'password')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    }
+    );
+  });
+});
