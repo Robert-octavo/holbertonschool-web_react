0ebf5b7ee04fe35e56f75ea5081adbf8dee477bc
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-19 22:24:00 -0500

    task 1. Use Immutable for the UI Reduce

--- 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,4 +1,12 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from "../actions/uiActionTypes";
+import { 
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE
+} from "../actions/uiActionTypes";
+import { Map } from 'immutable';
 
 const initialState = {
   isNotificationDrawerVisible: false,
@@ -6,43 +14,18 @@ const initialState = {
   user: {},
 };
 
-/*
-create a reducer function named uiReducer:
-
-    DISPLAY_NOTIFICATION_DRAWER should set isNotificationDrawerVisible to true
-    HIDE_NOTIFICATION_DRAWER should set isNotificationDrawerVisible to false
-    LOGIN_SUCCESS should set isUserLoggedIn to true
-    LOGIN_FAILURE should set isUserLoggedIn to false
-    LOGOUT should set isUserLoggedIn to false
-*/
-
-export function uiReducer(state = initialState, action) {
+export default function uiReducer(state = Map(initialState), action) {
   switch (action.type) {
     case DISPLAY_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-        isNotificationDrawerVisible: true,
-      };
+      return state.set('isNotificationDrawerVisible', true);
     case HIDE_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-        isNotificationDrawerVisible: false,
-      };
+      return state.set('isNotificationDrawerVisible', false);
     case LOGIN_SUCCESS:
-      return {
-        ...state,
-        isUserLoggedIn: true,
-      };
+      return state.set('isUserLoggedIn', true);
     case LOGIN_FAILURE:
-      return {
-        ...state,
-        isUserLoggedIn: false,
-      };
+      return state.set('isUserLoggedIn', false);
     case LOGOUT:
-      return {
-        ...state,
-        isUserLoggedIn: false,
-      };
+      return state.set('isUserLoggedIn', false);
     default:
       return state;
   }
