2bd14fd360e4a1f0e17f4d92d1e2de75eeca5e0a
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-19 22:48:01 -0500

    task 3 - Create a load action

--- 0x09-react_redux_reducer_selector/task_3/dashboard/src/actions/notificationActionTypes.js
+++ 0x09-react_redux_reducer_selector/task_3/dashboard/src/actions/notificationActionTypes.js
@@ -1,5 +1,6 @@
 export const MARKS_AS_READ = 'MARKS_AS_READ';
 export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
+export const FETCH_NOTIFICATIONS_SUCCESS = 'FETCH_NOTIFICATIONS_SUCCESS';
 
 export const NotificationTypeFilters = {
   DEFAULT: 'DEFAULT',
