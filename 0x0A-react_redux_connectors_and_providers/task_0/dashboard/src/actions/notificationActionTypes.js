96b0843368c4b7e0fefee659639028676740ef6d
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-22 21:19:58 -0500

    task 0 - files

---
+++ 0x0A-react_redux_connectors_and_providers/task_0/dashboard/src/actions/notificationActionTypes.js
@@ -0,0 +1,8 @@
+export const MARKS_AS_READ = 'MARKS_AS_READ';
+export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
+export const FETCH_NOTIFICATIONS_SUCCESS = 'FETCH_NOTIFICATIONS_SUCCESS';
+
+export const NotificationTypeFilters = {
+  DEFAULT: 'DEFAULT',
+  URGENT: 'URGENT'
+};
