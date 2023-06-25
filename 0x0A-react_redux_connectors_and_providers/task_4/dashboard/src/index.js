7f41ce7ff73cd3eaba9c4ea733f57b84644af739
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-24 21:00:06 -0500

    task 15. Combine store: modify the application

--- 0x0A-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { legacy_createStore as createStore} from 'redux';
-import { applyMiddleware, compose } from 'redux';
+import { applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import uiReducer, { initialState } from './reducers/uiReducer';
-import { Map } from 'immutable';
-//import './index.css';
+import rootReducer, { initialState} from './reducers/rootReducer';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import thunk from 'redux-thunk';
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(uiReducer, Map(initialState), enhancer(applyMiddleware(thunk)));
+const store = createStore(combineReducers(rootReducer), initialState, enhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
