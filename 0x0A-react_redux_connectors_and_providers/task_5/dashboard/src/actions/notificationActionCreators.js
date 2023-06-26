602d141030081c9cdc7467d8a2fe1c57a0b4d783
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-25 19:37:51 -0500

    task 17. Connect notifications: New Action Creator

--- 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
+++ 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -1,11 +1,37 @@
-import { MARKS_AS_READ, SET_TYPE_FILTER, NotificationTypeFilters } from "./notificationActionTypes";
+import { 
+  MARKS_AS_READ, 
+  SET_TYPE_FILTER,
+  SET_LOADING_STATE,
+  FETCH_NOTIFICATIONS_SUCCESS,
+} from "./notificationActionTypes";
 
 export const markAsAread = (index) => ({
   type: MARKS_AS_READ,
-  index
+  index,
 });
 
 export const setNotificationFilter = (filter) => ({
   type: SET_TYPE_FILTER,
-  filter
+  filter,
 });
+
+export const setLoadingState = (loading) => ({
+  type: SET_LOADING_STATE,
+  loading,
+});
+
+export const setNotifications = (data) => ({
+  type: FETCH_NOTIFICATIONS_SUCCESS,
+  data,
+});
+
+export const fetchNotifications = () => {
+  return (dispatch) => {
+    dispatch(setLoadingState(true));
+    return fetch('./notifications.json')
+      .then((res) => res.json())
+      .then((data) => dispatch(setNotifications(data)))
+      .catch((err) => console.log(err))
+      .finally(() => dispatch(setLoadingState(false)));
+  };
+};
--- 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionTypes.js
+++ 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionTypes.js
@@ -1,6 +1,7 @@
 export const MARKS_AS_READ = 'MARKS_AS_READ';
 export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
 export const FETCH_NOTIFICATIONS_SUCCESS = 'FETCH_NOTIFICATIONS_SUCCESS';
+export const SET_LOADING_STATE = 'SET_LOADING_STATE';
 
 export const NotificationTypeFilters = {
   DEFAULT: 'DEFAULT',
