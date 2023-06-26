165600e6b22abdf6502878d4e99ebcbeb974f701
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-06-25 19:41:51 -0500

    task 18. Connect notifications: Improve reducer

--- 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -8,7 +8,8 @@ import { Map, setIn } from "immutable";
 
 const initialState = {
   notifications:[],
-  filter: "DEFAULT"
+  filter: "DEFAULT",
+  loading: false,
 };
 
 export default function notificationReducer(state = Map(initialState), action) {
@@ -23,6 +24,8 @@ export default function notificationReducer(state = Map(initialState), action) {
       return setIn(state, ["notifications", action.index, "isRead"], true);
     case SET_TYPE_FILTER:
       return state.set("filter", action.filter);
+    case "SET_LOADING_STATE":
+      return state.set("loading", action.loading);
     default:
       return state;
   }
